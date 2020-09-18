b655b70fc86797cf1a2b9546005e9b5278edbb4e
Lee Nelson <lnelson@nelnet.org>
2020-09-18 16:15:27 -0700

    Add getopts and option for key type

--- client.sh
+++ client.sh
@@ -1,40 +1,67 @@
 #!/usr/local/bin/bash
 
-PRIVATE=$1      #path/filename for user private key
-CA=$2           #path/filename for ca private key
-KEYUSER=$3     #user to list as principal in cert
-KEYHOST=$4      #hostname of machine to create keys for
+usage() { echo "$0 usage:" && grep " .)\ #" $0; exit 0; }
+[ $# -eq 0 ] && usage
+while getopts ":hp:c:u:h:t:" arg; do
+  case $arg in
+      p) # path/filename for user private key
+      PRIVATE=${OPTARG}
+      ;;
+      c) # path/filename for ca private key
+      CA=${OPTARG}
+      ;;
+      u) # user to list as principal in cert
+      KEYUSER=${OPTARG}
+      ;;
+      h) # hostname of machine to create keys for
+      KEYHOST=${OPTARG}
+      ;;
+      t) # key type (rsa, ed25519)
+      TYPE=${OPTARG}
+      ;;
+      h | *) # Display help.
+	  usage
+	  exit 1
+	  ;;
+  esac
+done
 
 IDENTITY="$KEYUSER@$KEYHOST"     #string to use as identity in cert
 
+[[ -d $KEYHOST ]] || mkdir $KEYHOST
+
+OWD=`pwd`
+cd $KEYHOST
+
 PUBLIC=${PRIVATE}.pub
 CERT=${PRIVATE}-cert.pub
-REVOKED=revoked-keys
-APPEND=
-SIZE=4096
-TYPE=rsa
+REVOKED=revoked_keys
+OPTIONS=
 
-###Check for revoked keys file
-if [ -f $REVOKED ]; then
-    echo "Found $REVOKED"
-    APPEND=-u
+if [ "${TYPE}X" = "X" ]; then
+    TYPE=rsa
+fi
+
+if [ $TYPE = "rsa" ]; then
+    OPTIONS="-b 4096"
+elif [ $TYPE = "ed25519" ]; then
+    OPTIONS="-a 100"
+else
+    echo "Unknown key type: $TYPE"
+    exit 1
 fi
 
 ####Generate new key pair
-ssh-keygen -b 4096 -t rsa -C "$IDENTITY" -f $PRIVATE
+echo ssh-keygen $OPTIONS -t $TYPE -C "$IDENTITY" -f $PRIVATE
 
 ###Add public key (if exists) to revoked keys
 if [ -f $PUBLIC ]; then
     echo "Found $PUBLIC"
-    ssh-keygen -k -f $REVOKED $APPEND $PUBLIC
-fi
-
-####Add cert (if exists) to revoked keys
-if [ -f $CERT ]; then
-    echo "Found $CERT"
-    ssh-keygen -k -f $REVOKED $APPEND $CERT
+    cat $PUBLIC >> $REVOKED
 fi
 
 ####Sign public key
 ssh-keygen -s $CA -n $KEYUSER -I $IDENTITY $PUBLIC
 
+cd $OWD
+exit 0
